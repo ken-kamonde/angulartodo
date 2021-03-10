40ddf6816c687fe4d1a5135dbf57f8264faf1307
Kennedy Kamonde <githinjikams@gmail.com>
2021-03-10 10:59:04 +0300

    version 1 of angular todo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCBVVMP4kq9ekcc2A0zLxgfRf_BWf067Y8",
+    authDomain: "angular-todo-bc42d.firebaseapp.com",
+    projectId: "angular-todo-bc42d",
+    storageBucket: "angular-todo-bc42d.appspot.com",
+    messagingSenderId: "312178244565",
+    appId: "1:312178244565:web:d9ae1c737896ed89d8db5c"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCBVVMP4kq9ekcc2A0zLxgfRf_BWf067Y8",
+    authDomain: "angular-todo-bc42d.firebaseapp.com",
+    projectId: "angular-todo-bc42d",
+    storageBucket: "angular-todo-bc42d.appspot.com",
+    messagingSenderId: "312178244565",
+    appId: "1:312178244565:web:d9ae1c737896ed89d8db5c"
+  }
 };
 
 /*
